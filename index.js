9d52fef4718dc62aa760cf95a547195718656903
mohammad-dn <mohammaddehghani2677@gmail.com>
2022-07-01 15:59:09 +0300

    first js commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const userBirthDay = prompt("whats your birthday")
+const userCurrentAge = 2022 - userBirthDay
+const c56= userCurrentAge + 56
+console.log(c56)
+switch (userCurrentAge) {
+    case 18:
+        console.log("shoma 18 sale hastid");
+        break
+    case 19:
+        console.log("shoma 19 sale hastid");
+        break
+    case 20:
+        console.log("shoma 20 sale hastid");
+        break
+    case 21:
+        console.log("shoma 21 sale hastid");
+        break
+    case 22:
+        console.log("shoma 22 sale hastid")
+        break
+    default:
+        console.log("sene shoma mojaz nmibashad")
+
+}
+
